5cb44fa58756c3a36fbe8805b08b495835827eb0
eternium238 <maslenkov.alexey2006@gmail.com>
2026-02-27 15:00:10 +0300

    MakeWork

---
+++ lab31.js
@@ -0,0 +1,14 @@
+let rand1 = Math.random() * 101;
+let r1 = parseInt(rand1);
+let rand2 = Math.random() * 101;
+let r2 = parseInt(rand2);
+let r = prompt(r1+ " * " + r2 + " = ?", "0");
+let res = parseInt(r);
+if(Number.isInteger(res))
+{
+    if(res == r1*r2)
+    {
+        alert("Вы знаете арифметику");
+    } else alert("Вы не знаете арифметику");
+} else alert("Вы не знаете цифры");
+
---
+++ script.js
@@ -0,0 +1,3 @@
+let r = prompt("Введите радиус","0");
+let rad = parseFloat(r);
+alert("Площадь круга: " + 3.1415 * rad*rad);
